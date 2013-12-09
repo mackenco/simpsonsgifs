33c58b1f35eadd261a5f36eccfbacc192c9c31db
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-12-08 19:49:50 -0500

    some progress

--- cli_test.rb
+++ cli_test.rb
@@ -1,21 +1,24 @@
-# path =  'vlc -vvv /Users/cmackenzie/Desktop/simpsons/episode.m4v'
-# exec path
+require 'rmagick'
+include Magick
 
-cmd_arr = ['/Applications/VLC.app/Contents/MacOS/VLC',
-           '-Idummy',
-           '--video-filter',
-           'scene',
-           '-Vdummy',
-           '--no-audio',
-           '--scene-height=256',
-           '--scene-width=512',
-           '--scene-format=png',
-           '--scene-ratio=1',
-           '--start-time=10',
-           '--stop-time=11',
-           '--scene-prefix=thumb',
-           '--scene-path=/Users/cmackenzie/Desktop/simpsons/output',
-           '/Users/cmackenzie/Desktop/simpsons/episode.m4v',
-           'vlc://quit']
-cmd = cmd_arr.join(' ')
+rando = Random.new.rand * 1000
+cmd = ['/Applications/VLC.app/Contents/MacOS/VLC',
+       '-Idummy',
+       '--video-filter',
+       'scene',
+       '-Vdummy',
+       '--no-audio',
+       '--scene-height=256',
+       '--scene-width=512',
+       '--scene-format=png',
+       '--scene-ratio=1',
+       "--start-time=#{rando}",
+       "--stop-time=#{rando + 2}",
+       '--scene-prefix=thumb',
+       '--scene-path=/Users/cmackenzie/Desktop/simpsons/output',
+       '/Users/cmackenzie/Desktop/simpsons/episode.m4v',
+       'vlc://quit'].join(' ')
 exec cmd
+
+animation = ImageList.new("/Users/cmackenzie/Desktop/simpsons/output/thumb00030.png")
+animation.display
