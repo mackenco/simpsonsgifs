77787885f477679bb9f9e280d520102ae5cc8fd7
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-12-09 23:10:23 -0500

    methods

--- cli_test.rb
+++ cli_test.rb
@@ -1,34 +1,43 @@
 require 'rmagick'
 include Magick
 
-rando = Random.new.rand * 1000
-cmd = ['/Applications/VLC.app/Contents/MacOS/VLC',
-       '-Idummy',
-       '--video-filter',
-       'scene',
-       '-Vdummy',
-       '--no-audio',
-       '--scene-height=256',
-       '--scene-width=512',
-       '--scene-format=png',
-       '--scene-ratio=1',
-       "--start-time=#{rando}",
-       "--stop-time=#{rando + 2}",
-       '--scene-prefix=thumb',
-       '--scene-path=/Users/cmackenzie/Desktop/projects/simpsons',
-       '/Users/cmackenzie/Desktop/projects/simpsons/episode.m4v',
-       'vlc://quit'].join(' ')
-system(cmd)
+def run
+  rando = Random.new.rand * 1000
+  cmd = ['/Applications/VLC.app/Contents/MacOS/VLC',
+         '-Idummy',
+         '--video-filter',
+         'scene',
+         '-Vdummy',
+         '--no-audio',
+         '--scene-height=256',
+         '--scene-width=512',
+         '--scene-format=png',
+         '--scene-ratio=1',
+         "--start-time=#{rando}",
+         "--stop-time=#{rando + 2}",
+         '--scene-prefix=thumb',
+         '--scene-path=/Users/cmackenzie/Desktop/projects/simpsons',
+         '/Users/cmackenzie/Desktop/projects/simpsons/episode.m4v',
+         'vlc://quit'].join(' ')
+  system(cmd)
 
-file = "00001"
+  file = "00001"
 
-# while File.exists?("thumb" + file + ".png")
-#   File.delete("thumb" + file + ".png") if File.zero?("thumb" + file + ".png")
-#   file.next!
-#   p file
-# end
+  p fileName(file)
+  # while File.exists?("thumb" + file + ".png")
+  #   File.delete("thumb" + file + ".png") if File.zero?("thumb" + file + ".png")
+  #   file.next!
+  #   p file
+  # end
 
-# 
-# animation = ImageList.new(*Dir["*.png"])
-# animation.delay = 10
-# animation.write("animated.gif")
+  # 
+  # animation = ImageList.new(*Dir["*.png"])
+  # animation.delay = 10
+  # animation.write("animated.gif")
+end
+
+def fileName(num)
+  "thumb" + num + ".png"
+end
+
+run
