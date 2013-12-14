975cfebff2d4ff490daaea92dc69885d125efa24
Colin MacKenzie <colinwmackenzie@gmail.com>
2013-12-14 10:53:55 -0500

    now supports entire season

--- gif_maker.rb
+++ gif_maker.rb
@@ -3,13 +3,10 @@ require 'srt'
 include Magick
 
 def run  
-  subs = SRT::File.parse(File.new("s07e01.srt"))  
+  ep = (1..25).to_a.sample
+  subs = SRT::File.parse(File.new("srts/S07E#{ep}.srt"))  
   
-  line = subs.lines[4..-1].sample #getting rid of some non-quote subs with the 3..-1  
-
-  p line.text
-  p line.start_time
-  p line.end_time
+  line = subs.lines[4..-1].sample #getting rid of some non-quote subs with the 4..-1  
   
   cmd = ['/Applications/VLC.app/Contents/MacOS/VLC',
          '-Idummy',
@@ -25,7 +22,7 @@ def run
          "--stop-time=#{line.end_time}",
          '--scene-prefix=thumb',
          '--scene-path=/Users/cmackenzie/Desktop/projects/simpsons',
-         '/Users/cmackenzie/Desktop/projects/simpsons/s07e01.m4v',
+         "/Users/cmackenzie/Desktop/projects/simpsons/eps/SIMPSONS7_#{ep}.m4v",
          'vlc://quit'].join(' ')
   system(cmd)
   
@@ -40,8 +37,8 @@ def run
   #had to write the text onto each individual frame of the gif; couldn't figure how to put the text onto the entire file
   animation.each do |frame|
     text = Magick::Draw.new
-    text.annotate(frame, 0, 0, 0, 0, line.text.join("\n")) {
-      self.font = 'DejaVuSansCondensed-BoldOblique.ttf'
+    text.annotate(frame, 0, 0, 0, 0, line.text.join("\n").gsub(/<.{1,2}>/, "")) { #eliminate <i></i> from some lines
+      self.font = 'fonts/DejaVuSansCondensed-BoldOblique.ttf'
       self.gravity = Magick::SouthGravity
       self.pointsize = 16
       self.stroke = 'transparent'
